9bd8724664b6481e51f3d007bc6a2b75ddea0e17
Jiya Hasan <40819160+jiyaaa@users.noreply.github.com>
2019-02-01 02:48:36 +0600

    Create Diploma_CGPA_BD.py

---
+++ Diploma_CGPA_BD.py
@@ -0,0 +1,14 @@
+print('System of 2016, Diploma In Engineering')
+I = (float(input('1st semester: '))*5)/100      
+II = (float(input('2nd Semester: '))*5)/100  
+III = (float(input('3rd Semester: '))*5)/100  
+IV = (float(input('4th Semester: '))*10)/100  
+V = (float(input('5th Semester: '))*15)/100  
+VI = (float(input('Six Semester: '))*20)/100  
+VII = (float(input('Seven Semester: '))*25)/100  
+VIII = (float(input('Eight Semester: '))*15)/100  
+
+Total_CGPA = I+II+III+IV+V+VI+VII+VIII
+print('Total CGPA:', Total_CGPA)
+
+#Coded by Jiya Hasan
