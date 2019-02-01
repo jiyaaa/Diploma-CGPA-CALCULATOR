2355ef00bbba4ed07fe42402512b843860cffddf
Jiya Hasan <40819160+jiyaaa@users.noreply.github.com>
2019-02-01 19:36:54 +0600

    Update Diploma_CGPA_BD.py

--- Diploma_CGPA_BD.py
+++ Diploma_CGPA_BD.py
@@ -1,14 +1,14 @@
 print('System of 2016, Diploma In Engineering')
-I = (float(input('1st semester: '))*5)/100      
-II = (float(input('2nd Semester: '))*5)/100  
-III = (float(input('3rd Semester: '))*5)/100  
-IV = (float(input('4th Semester: '))*10)/100  
-V = (float(input('5th Semester: '))*15)/100  
-VI = (float(input('Six Semester: '))*20)/100  
-VII = (float(input('Seven Semester: '))*25)/100  
-VIII = (float(input('Eight Semester: '))*15)/100  
+I = (float(input('1st semester: '))*5)      
+II = (float(input('2nd Semester: '))*5) 
+III = (float(input('3rd Semester: '))*5)  
+IV = (float(input('4th Semester: '))*10) 
+V = (float(input('5th Semester: '))*15)  
+VI = (float(input('Six Semester: '))*20)  
+VII = (float(input('Seven Semester: '))*25)  
+VIII = (float(input('Eight Semester: '))*15)  
 
-Total_CGPA = I+II+III+IV+V+VI+VII+VIII
+Total_CGPA = (I+II+III+IV+V+VI+VII+VIII)/100
 print('Total CGPA:', Total_CGPA)
 
 #Coded by Jiya Hasan
